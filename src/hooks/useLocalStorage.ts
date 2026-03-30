86e39f11d53ee5373a5494ca009ed61631beccb1
tera-the <tera2000b@gmail.com>
2026-03-30 16:52:24 +0500

    update

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,36 +1,35 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-    const [value, setValue] = useState(() => {
+    const [value, setValue] = useState<T>(() => {
         try {
             const jsonValue = localStorage.getItem(key);
 
             if (jsonValue !== null) return JSON.parse(jsonValue);
 
             if (typeof initialValue === 'function') {
-                return (initialValue as () => T)()
+                return (initialValue as () => T)();
             }
 
             return initialValue;
         } catch (e) {
-            console.error("Error writing localStorage", e)
+            console.error("Error reading localStorage", e);
 
             if (typeof initialValue === 'function') {
-                return (initialValue as () => T)()
+                return (initialValue as () => T)();
             }
 
             return initialValue;
         }
-
-    })
+    });
 
     useEffect(() => {
         try {
             localStorage.setItem(key, JSON.stringify(value));
         } catch (e) {
-            console.error("Error writing localStorage", e)
+            console.error("Error writing localStorage", e);
         }
-    }, [key, value])
+    }, [key, value]);
 
     return [value, setValue] as const;
 }
